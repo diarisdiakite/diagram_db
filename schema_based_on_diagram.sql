b967c8569d9e3f4dada2e63ca5595d853326f57e
MHNazary <mhnazary2020@gmail.com>
2023-09-27 05:29:54 +0430

    Added remain table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,3 +11,35 @@ CREATE TABLE medical_histories (
   status VARCHAR(150)
 );
 
+-- treatments table
+CREATE TABLE treatments (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(50),
+    name VARCHAR(80)
+);
+
+--invoices table
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    total_amount FLOAT,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT FOREIGN KEY REFERENCES medical_histories(id)
+);
+
+--invoice_items table
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price FLOAT,
+    quantity INT,
+    total_price FLOAT,
+    invoice_id int FOREIGN KEY REFERENCES invoices(id),
+    treatment_id int FOREIGN KEY REFERENCES treatments(id)
+);
+
+-- treatments_medical_histories table
+CREATE TABLE treatments_medical_histories (
+    treatment_id INT FOREIGN KEY REFERENCES treatments(id),
+    medical_history_id int FOREIGN KEY REFERENCES medical_histories(id),
+    PRIMARY KEY (treatment_id, medical_history_id)
+);
